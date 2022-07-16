d779eeff878e4b1c1dcd97ca636e23f6d1d6e97e
Attila Palfi <attila.palfi.92@gmail.com>
2022-07-16 11:02:29 +0200

    added unit tests for charge services

--- src/main/kotlin/com/katanox/api/charges/percentage/PercentageChargeRepository.kt
+++ src/main/kotlin/com/katanox/api/charges/percentage/PercentageChargeRepository.kt
@@ -2,17 +2,16 @@ package com.katanox.api.charges.percentage
 
 import com.katanox.test.sql.tables.ExtraChargesPercentage.EXTRA_CHARGES_PERCENTAGE
 import org.jooq.DSLContext
-import org.jooq.Record
-import org.jooq.Result
 import org.springframework.stereotype.Repository
 
 @Repository
 class PercentageChargeRepository(private val dsl: DSLContext) {
 
-    fun findChargesByHotel(hotelId: Long): Result<Record> {
+    fun findChargesByHotel(hotelId: Long): Set<PercentageChargeDto> {
         return dsl.select()
             .from(EXTRA_CHARGES_PERCENTAGE)
             .where(EXTRA_CHARGES_PERCENTAGE.HOTEL_ID.equal(hotelId))
             .fetch()
+            .map { PercentageChargeDto.ofRecord(it) }.toSet()
     }
 }
